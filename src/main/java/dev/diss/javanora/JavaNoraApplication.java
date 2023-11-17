ad8db366b45ec105fc5181b7e49a863224699d42
diss <65290988+Dissurender@users.noreply.github.com>
2023-11-17 07:01:56 -0600

    bork

---
+++ src/main/java/dev/diss/javanora/JavaNoraApplication.java
@@ -0,0 +1,13 @@
+package dev.diss.javanora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diss/javanora/JavaNoraApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diss.javanora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
